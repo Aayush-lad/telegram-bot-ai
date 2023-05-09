0f13197ec9ec345ce873e966a09846903e85ebe5
Aayush Lad <aayushlad1973@gmail.com>
2023-05-09 21:43:36 +0530

    telegram-chatbot

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
+
 const TelegramBot = require('node-telegram-bot-api');
-const {Configuration,OpenAIApi} = require('openai');
-require('dotenv').config();
-const TOKEN=process.env.TOKEN
+const { Configuration, OpenAIApi } = require('openai');
+const TOKEN = '5947855269:AAHdMBIPiFqLKSi8WP1RpKT544s5M_b2C8c';
+const express = require('express');
+const app = express();
+app.get('/', (req, resp) => {
+  resp.send('Hello world');
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log("server is running at port ")
+})
 const bot = new TelegramBot(TOKEN, { polling: true });
-const key=process.env.OPENAI_API_KEY;
-const org=process.env.ORG;
+const key = 'skBIqCVhG5LXspSSTY3NyoT3BlbkFJiHbFpPHPj8Ck8Bs7MOHy';
+const org = 'org-JkZ0LUNKNGucB22m3Xj2oe3X';
 console.log("chat bot started");
-const configuration=new Configuration({
-    organization:org ,
-    apiKey: key
+const configuration = new Configuration({
+  organization: org,
+  apiKey: key
 })
 const openai = new OpenAIApi(key);
-bot.on('message',async function(msg){
-    const chatId = msg.chat.id;
-    const input = msg.text;
-    const openai= new OpenAIApi(configuration);
-    const response = await openai.createCompletion({
-        prompt:input,
-        model: "text-davinci-003",
-        max_tokens: 500,
-        temperature:0,
-        top_p:1,
-        frequency_penalty:0.5,
-        presence_penalty:0,
-    })
+bot.on('message', async function(msg) {
+  const chatId = msg.chat.id;
+  const input = msg.text;
+  const openai = new OpenAIApi(configuration);
+  const response = await openai.createCompletion({
+    prompt: input,
+    model: "text-davinci-003",
+    max_tokens: 500,
+    temperature: 0,
+    top_p: 1,
+    frequency_penalty: 0.5,
+    presence_penalty: 0,
+  })
 
-    bot.sendMessage(chatId, `${response.data.choices[0].text}`);
+  bot.sendMessage(chatId, `${response.data.choices[0].text}`);
 })
+
